a11ce4286c103fbd96066705bd0d58b6df3e6c55
AWS_Devops <awsdevops7779@gmail.com>
2026-03-21 16:34:13 +0530

    Modify devops weekend session greeting
    Updated greeting message for the devops weekend session.

--- learn.py
+++ learn.py
@@ -1,7 +1,5 @@
 print("hello devops class")
 
-print("hello devops weekend session")
-
-print("hello devops weekend session")
+print("hello devops weekend session v1.0")
 
 print("test for SCM Poll")
